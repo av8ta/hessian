875b6eb3204549b4bfbe6856779968bd3d45d840
av8ta <av8ta@pm.me>
2023-01-06 22:31:38 +0800

    add ability for watch script to run one test file

--- watch.sh
+++ watch.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # https://stackoverflow.com/a/22885210 thanks!
 
+# ./watch.sh to watch all *.test.gr and *.gr files
+#     - runs all tests in test.sh
+# ./watch.sh path/to/test/to/run.test.gr
+
 watched_files=$(find . -name '*.gr')
 
 if [ -z "$watched_files" ]; then
@@ -14,7 +18,17 @@ previous_checksum="dummy"
 while true; do
   checksum=$(md5sum $watched_files | md5sum) # use `md5` on Mac, in linux it's `md5sum`
   if [ "$checksum" != "$previous_checksum" ]; then
-    ./test.sh
+    if [ "$1" ]; then
+      grain "$1"
+      ret=$?
+      # echo "Command exited with $ret"
+      if [ $ret -ne 0 ]; then
+        echo "Failed. Restarting test"
+        continue
+      fi
+    else
+      ./test.sh
+    fi
   fi
   previous_checksum="$checksum"
   sleep 1
